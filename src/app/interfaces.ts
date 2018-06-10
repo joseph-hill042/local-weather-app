3a8badbee05c4cdc4e175f97a30b7681885949c9
Joseph Hill <joseph.hill1@outlook.com>
2018-06-09 22:49:28 -0400

    Corrected e2e and unit tests to passing

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,12 +1,12 @@
 /**
  * @Interface CurrentWeather
  * Represents the current weather
- * @property city: string
- * @property country: string
- * @property date: number
- * @property image: string
- * @property temperature: number
- * @property description: string
+ * @property city string
+ * @property country string
+ * @property date number
+ * @property image string
+ * @property temperature number
+ * @property description string
  */
 export interface ICurrentWeather {
   city: string
