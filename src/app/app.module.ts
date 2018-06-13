a8167da96804a2da2ef48164d32857193c72f9a3
Joseph Hill <joseph.hill1@outlook.com>
2018-06-12 22:12:29 -0400

    Added material and flex-layout to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 import { WeatherService } from './weather/weather.service'
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+} from '@angular/material'
+
+@NgModule({
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  declarations: [],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+})
+export class MaterialModule {}
