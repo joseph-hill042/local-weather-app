d4f28ed6677cfb7a84b107d2170071f69da2706c
Joseph Hill <joseph.hill1@outlook.com>
2018-06-21 18:15:04 -0400

    Corrected unit tests to passing by including material module.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 import { WeatherService } from './weather/weather.service'
 import { WeatherServiceFake } from './weather/weather.service.fake'
+import { MaterialModule } from './material.module'
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent, CurrentWeatherComponent],
       providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
+      imports: [MaterialModule],
     }).compileComponents()
   }))
   it('should create the app', async(() => {
