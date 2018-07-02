eb96728b7319b6c0e564064cce08314734fc1775
Joseph Hill <joseph.hill1@outlook.com>
2018-07-02 11:17:14 -0400

    Implemented functionality of search to change weather data displayed to data returned from search.

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, Validators } from '@angular/forms'
 import { WeatherService } from '../weather/weather.service'
 import { debounceTime } from 'rxjs/operators'
 import { FormErrorService } from '../forms/form-error.service'
+import { ICurrentWeather } from '../interfaces'
 
 @Component({
   selector: 'app-city-search',
@@ -12,19 +13,22 @@ import { FormErrorService } from '../forms/form-error.service'
 export class CitySearchComponent implements OnInit {
   search = new FormControl('', [Validators.minLength(2)])
   constructor(private weatherService: WeatherService) {}
-
+  @Output()
+  weatherSearch: EventEmitter<ICurrentWeather> = new EventEmitter(true)
   ngOnInit() {
     this.search.valueChanges
       .pipe(debounceTime(1000))
       .subscribe((searchValue: string) => {
-        if (searchValue) {
+        if (searchValue && !this.search.invalid) {
           const userInput = searchValue.split(',').map(s => s.trim())
           this.weatherService
             .getCurrentWeather(
               userInput[0],
               userInput.length > 1 ? userInput[1] : undefined
             )
-            .subscribe(data => console.log(data))
+            .subscribe(data => {
+              this.weatherSearch.emit(data)
+            })
         }
       })
   }
